c0add643b16506695c1f9a2290520c71869448be
Sakhare <sakanand@amazon.com>
2020-04-20 23:22:38 -0400

    Add permission file code

--- create_permissions_file.py
+++ create_permissions_file.py
@@ -1,9 +1,14 @@
 def create_permissions_file(list_of_emp_ids, number_of_levels):
-    output_header=["CEO_" + str(i) if i!=0 else 'CEO' for i in range(number_of_levels)]
-    output_header.insert(0,'Employee_id')
+    output_header=["ceo_" + str(i) if i!=0 else 'ceo' for i in range(number_of_levels)]
+    output_header.insert(0,'UserName')
     f=open("./sample_permissions_file.csv", 'w')
     writer = csv.writer(f)
     writer.writerow(output_header)
     for i in list_of_emp_ids:
-        writer.writerow([i]*(number_of_levels+2))
+        for j in range(1,len(output_header)):
+            l = [None] * (len(output_header))
+            l[j]=i
+            l[0]=i
+            print(l)
+            writer.writerow(l)
     f.close()
